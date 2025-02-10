a8a91342028e723365216e0ed48b7f3b08a6efbe
Jmlv7 <165213783+Jmlv7@users.noreply.github.com>
2025-02-10 08:20:30 -0300

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Rectangle {
+    private double largura;
+    private double altura;
+
+    // Getters
+    public double getLargura() {
+        return largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    // Setters
+    public void setLargura(double largura) {
+        if (largura > 0) {
+            this.largura = largura;
+        }
+    }
+
+    public void setAltura(double altura) {
+        if (altura > 0) {
+            this.altura = altura;
+        }
+    }
+
+    public double area() {
+        return largura * altura;
+    }
+
+    public double perimetro() {
+        return 2 * (largura + altura);
+    }
+
+    public double diagonal() {
+        return Math.sqrt(largura * largura + altura * altura);
+    }
 }
